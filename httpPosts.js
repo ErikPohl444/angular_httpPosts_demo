0fda4ef2b0286ae3c067007433d94aad81fe7017
Erik Pohl <epohl@dmigs.com>
2024-11-19 12:28:24 -0500

    Added a way to track ids of posts when changes occur in the select

--- httpPosts.js
+++ httpPosts.js
@@ -5,4 +5,8 @@ app.controller("httpController", function($scope, $http) {
   .success(function(result) {
   	$scope.posts = result;
   });
+  
+  $scope.handleChange = function(postingid) {
+  	alert("post has changed!" + postingid);
+  }
 });
